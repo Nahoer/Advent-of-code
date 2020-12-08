75907e956d3b41349e2b6b91fae71ee3f5508806
Prunot Clément <clem.prunot@gmail.com>
2020-12-08 10:25:10 +0100

    Add sln

--- Advent-of-code/src/Day/8/main8.cs
+++ Advent-of-code/src/Day/8/main8.cs
@@ -9,7 +9,7 @@ namespace Advent_of_code.src.Day._8
     {
         public static void Start()
         {
-            string[] lines = System.IO.File.ReadAllLines(Directory.GetCurrentDirectory().Replace(@"bin\Debug\netcoreapp3.1", @"src\Day\8\input.txt"));
+            string[] lines = System.IO.File.ReadAllLines(Directory.GetCurrentDirectory().Replace(@"bin\Debug\netcoreapp3.1", @"src\Day\8\input.txt")); 
             GameConsole g = new GameConsole();
             Console.WriteLine(g.RunScript(lines, 0)); 
             
